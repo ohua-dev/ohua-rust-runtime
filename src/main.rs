371fd3c836d3547aed2e7e52d86bd6c599561920
Felix Wittwer <dev@felixwittwer.de>
2017-11-03 11:13:53 +0100

    Implement JSON deserialisation for ohuac files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+#[macro_use]
+extern crate clap;
+
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+
+mod types;
+
+use std::fs::File;
+use clap::{App, Arg};
+use types::OhuaData;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("ohua-rust-runtime")
+        .version(crate_version!())
+        .author("Felix Wittwer <dev@felixwittwer.de>")
+        .about("Rust runtime generation from Ohua code.")
+        .arg(Arg::with_name("input")
+                 .help("Ohua object file in JSON format.")
+                 .required(true)
+                 .takes_value(true))
+        .get_matches();
+
+    let path = matches.value_of("input").unwrap();
+    let file = File::open(path).unwrap();
+
+    let ohua_data: OhuaData = serde_json::from_reader(file).unwrap();
 }
---
+++ src/types.rs
@@ -0,0 +1,52 @@
+#![allow(non_snake_case)]
+// TODO: Maybe refactor the data structures (using serde-rename) to make the field names rust-compliant
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct OhuaData {
+    graph: DFGraph,
+    mainArity: i32,
+    sfDependencies: Vec<SfDependency>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DFGraph {
+    operators: Vec<Operator>,
+    arcs: Vec<Arc>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Operator {
+    operatorId: i32,
+    operatorType: OperatorType,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct OperatorType {
+    qbNamespace: Vec<String>,
+    qbName: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Arc {
+    target: ArcIdentifier,
+    source: ArcSource,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ArcIdentifier {
+    operator: i32,
+    index: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ArcSource {
+    #[serde(rename(deserialize = "type"))]
+    s_type: String,
+    val: ArcIdentifier,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SfDependency {
+    qbNamespace: Vec<String>,
+    qbName: String,
+}
