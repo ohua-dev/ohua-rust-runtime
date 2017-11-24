3ef2024255402cf2d57dfe46072c60196998e93a
Felix Wittwer <dev@felixwittwer.de>
2017-11-24 15:46:19 +0100

    Refactor test case to make op spawn generic

--- test-resources/manual-test-case/src/main.rs
+++ test-resources/manual-test-case/src/main.rs
@@ -2,11 +2,13 @@
 
 mod types;
 mod hello;
+mod runtime;
 use types::*;
+use runtime::*;
 
 use std::thread;
 use std::sync::mpsc;
-// use std::mem::transmute;
+
 
 fn generate_dummy() -> OhuaData {
     OhuaData {
@@ -63,25 +65,6 @@ fn generate_dummy() -> OhuaData {
     }
 }
 
-// introduced because Rust does not allow Trait implementations for types that are defined elsewhere
-#[derive(Debug)]
-struct GenericType {}
-
-
-impl From<Box<GenericType>> for Box<i32> {
-    fn from(arg: Box<GenericType>) -> Self {
-        // raw pointer cast. Quick and dirty. The joke is that the pointer cast
-        // itself is ok as long as both types implement the `Sized` trait (I guess).
-        // Box::from_raw is the unsafe kiddo
-        unsafe { Box::from_raw(Box::into_raw(arg) as *mut i32) }
-    }
-}
-
-impl From<Box<i32>> for Box<GenericType> {
-    fn from(arg: Box<i32>) -> Self {
-        unsafe { Box::from_raw(Box::into_raw(arg) as *mut GenericType) }
-    }
-}
 
 fn calc_wrapped(mut args: Vec<Box<GenericType>>) -> Vec<Box<GenericType>> {
     let arg1 = Box::from(args.pop().unwrap());
@@ -104,29 +87,46 @@ fn main() {
     // let's just assume this was somehow extracted/generated
     let deserialized_data = generate_dummy();
 
+    // instantiate the operator(s)
+    let mut operators: Vec<OhuaOperator> = Vec::new();
+
+    // imagine this to be a for-loop someday
+    operators.push(OhuaOperator {
+                       input: vec![],
+                       output: vec![],
+                       func: calc_wrapped,
+                   });
+    operators.push(OhuaOperator {
+                       input: vec![],
+                       output: vec![],
+                       func: world_wrapped,
+                   });
+
     // channel creation
     let (insertion_point, r1) = mpsc::channel();
+    operators[0].input.push(r1);
     let (s2, r2) = mpsc::channel();
+    operators[0].output.push(s2);
+    operators[1].input.push(r2);
     let (s3, output_port) = mpsc::channel();
+    operators[1].output.push(s3);
 
     // thread spawning
-    thread::spawn(move || {
-                      // receive
-                      let args = vec![r1.recv().unwrap()];
+    for op in operators.drain(..) {
+        thread::spawn(move || {
+            // receive
+            let mut args = vec![];
+            for recv in op.input {
+                args.push(recv.recv().unwrap());
+            }
 
-                      // call & send
-                      let mut results = calc_wrapped(args);
-                      s2.send(results.pop().unwrap()).unwrap();
-                  });
-
-    thread::spawn(move || {
-                      // receive
-                      let args = vec![r2.recv().unwrap()];
-
-                      // call & send
-                      let mut results = world_wrapped(args);
-                      s3.send(results.pop().unwrap()).unwrap();
-                  });
+            // call & send
+            let mut results = (op.func)(args);
+            for elem in results.drain(..).enumerate() {
+                op.output[elem.0].send(elem.1).unwrap();
+            }
+        });
+    }
 
     // providing input to the DFG
     insertion_point.send(Box::from(Box::new(3))).unwrap();
---
+++ test-resources/manual-test-case/src/runtime.rs
@@ -0,0 +1,30 @@
+use std::sync::mpsc::{Sender, Receiver};
+
+// introduced because Rust does not allow Trait implementations for types that are defined elsewhere
+#[derive(Debug)]
+pub struct GenericType {}
+
+impl From<Box<GenericType>> for Box<i32> {
+    fn from(arg: Box<GenericType>) -> Self {
+        // raw pointer cast. Quick and dirty. The joke is that the pointer cast
+        // itself is ok as long as both types implement the `Sized` trait (I guess).
+        // Box::from_raw is the unsafe kiddo
+        unsafe { Box::from_raw(Box::into_raw(arg) as *mut i32) }
+    }
+}
+
+impl From<Box<i32>> for Box<GenericType> {
+    fn from(arg: Box<i32>) -> Self {
+        unsafe { Box::from_raw(Box::into_raw(arg) as *mut GenericType) }
+    }
+}
+
+// TODO: Maybe move things from above to separate file (because it's dynamic)?
+
+// maybe extend this with a name?
+//#[derive(Debug)]
+pub struct OhuaOperator {
+    pub input: Vec<Receiver<Box<GenericType>>>,
+    pub output: Vec<Sender<Box<GenericType>>>,
+    pub func: fn(Vec<Box<GenericType>>) -> Vec<Box<GenericType>>,
+}
