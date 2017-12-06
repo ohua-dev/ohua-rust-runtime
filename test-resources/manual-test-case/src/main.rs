43072771370d5f723806c8ea5aec2df36c1e2a29
Felix Wittwer <dev@felixwittwer.de>
2017-12-06 17:29:33 +0100

    Start off with static channel generation

--- test-resources/manual-test-case/src/main.rs
+++ test-resources/manual-test-case/src/main.rs
@@ -12,15 +12,17 @@ use runtime::*;
 
 use std::thread;
 use std::sync::mpsc;
-
+use std::collections::HashMap;
 
 
 fn main() {
     // let's just assume this function will be generated
     let runtime_data = ohuadata::generate();
 
-    // instantiate the operator(s)
-    let mut operators: Vec<OhuaOperator> = Vec::new();
+    // TODO: Move the Arc generation here in order to be able to allocate enough space for the I/O channels when generating the operator struct
+
+    // instantiate the operator vector with space for exactly n operators
+    let mut operators: Vec<OhuaOperator> = Vec::with_capacity(runtime_data.graph.operators.len());
 
     // statically fill the operator struct
     for op in runtime_data.graph.operators {
@@ -41,14 +43,47 @@ fn main() {
     let (s3, output_port) = mpsc::channel();
     operators[1].output.push(s3);
 
+    let mut channels: Vec<(HashMap<u32, mpsc::Receiver<Box<GenericType>>>, HashMap<u32, Vec<mpsc::Sender<Box<GenericType>>>>)> = Vec::with_capacity(operators.len());
+
+    for _ in 0..operators.len() {
+        // are you fkin' serious
+        channels.push((HashMap::new(), HashMap::new()));
+    }
+
+    // TODO: Make Arc recognition Enum based(?)
     for arc in runtime_data
             .graph
             .arcs
             .iter()
-            .filter(|x| x.source.s_type == String::from("env")) {
+            .filter(|x| x.source.s_type == String::from("local")) {
         let (s, r) = mpsc::channel::<Box<GenericType>>();
-        // place channel correctly
+
+        // place the receiver
+        channels[(arc.target.operator - 1) as usize].0.insert(arc.target.index as u32, r);
+
+        // place the sender
+        if let types::ValueType::LocalVal(ref source) = arc.source.val {
+            let sender_index: u32 = if source.index >= 0 {
+                source.index as u32
+            } else {
+                0
+            };
+
+            if let Some(target) = channels[(source.operator - 1) as usize].1.get_mut(&sender_index) {
+                // if there is at least 1 sender in place to transmit the value, just add the sender
+                target.push(s);
+                continue;
+            }
+
+            // if no sender is available for that slot (yet), add a new one
+            channels[(source.operator - 1) as usize].1.insert(sender_index, vec![s]);
+        } else {
+            panic!("Encountered malformed ArcSource, type is `local` but type is EnvironmentVal.");
+        }
     }
+
+    println!("{:?}", channels);
+    // TODO: now put these into the right operators, (maybe) move upper part to function
 
     // thread spawning -- static
     for op in operators.drain(..) {
