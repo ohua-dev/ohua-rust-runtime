3c085100e2491a2c574c4f229fbb849d18adba81
Felix Wittwer <dev@felixwittwer.de>
2018-02-07 19:58:55 +0100

    Add cloning generation to wrapper generation
    - function wrappers are now generated using a 2-dimensional vec for
    output values
    - used `rustfmt` to reformat code and do some cleanup

--- src/typecasts.rs
+++ src/typecasts.rs
@@ -10,9 +10,13 @@ fn generate_for(types: HashSet<String>, target_file: &str) -> Result<()> {
     let typecast = include_str!("templates/snippets/typecast.in");
 
     let mut typecast_file = String::new();
-    
+
     for datatype in types {
-        typecast_file.push_str(typecast.replace("{target_type}", datatype.as_str()).as_str());
+        typecast_file.push_str(
+            typecast
+                .replace("{target_type}", datatype.as_str())
+                .as_str(),
+        );
     }
 
     File::create(target_file)?.write_fmt(format_args!("{}{}", generic_type_file, typecast_file))
@@ -22,12 +26,9 @@ fn get_argument_types(fn_name: String) -> Vec<String> {
     match fn_name.as_str() {
         "hello::calc" => vec![String::from("i32")],
         "hello::world" => vec![String::from("i32")],
-        "strings::gen_s1" => vec![String::from("i32")],
-        "strings::gen_s2" => vec![String::from("i32")],
-        "strings::append" => vec![String::from("String")],
-        "strings::duplicate" => vec![String::from("String")],
-        "strings::count" => vec![String::from("String")],
-        _ => vec![]
+        "strings::gen_string" => vec![String::from("i32")],
+        "strings::count_strings" => vec![String::from("String")],
+        _ => vec![],
     }
 }
 
@@ -36,11 +37,10 @@ pub fn generate_casts(operators: &Vec<Operator>, target_file: &str) -> Result<()
 
     for op in operators {
         let fn_name = op.operatorType
-                        .qbNamespace
-                        .iter()
-                        .fold(String::new(), |acc, ref x|
-                            acc.to_owned() + &x + "::"
-                        ) + op.operatorType.qbName.as_str();
+            .qbNamespace
+            .iter()
+            .fold(String::new(), |acc, ref x| acc.to_owned() + &x + "::")
+            + op.operatorType.qbName.as_str();
 
         for occuring_type in get_argument_types(fn_name) {
             used_types.insert(occuring_type);
--- src/wrappers.rs
+++ src/wrappers.rs
@@ -4,13 +4,13 @@ use std::fs::File;
 
 use types::*;
 
-pub fn wrap_function(name: &str, incoming_arcs: u16, outgoing_arcs: u16) -> String {
+pub fn wrap_function(name: &str, incoming_arcs: usize, mut outgoing_arcs: Vec<usize>) -> String {
     let mut skeleton = String::from(include_str!("templates/snippets/wrapper.in"));
     let unpack_arg = "let arg{n} = Box::from(args.pop().unwrap());\n";
 
     // set the wrapper function name
     skeleton = skeleton.replace("{escaped_name}", name.replace("::", "_").as_ref());
-    
+
     // incoming arguments
     let mut incoming = String::new();
     for i in 0..incoming_arcs {
@@ -27,32 +27,82 @@ pub fn wrap_function(name: &str, incoming_arcs: u16, outgoing_arcs: u16) -> Stri
         fn_args.push_str(format!("*arg{}", i).as_ref());
     }
 
-    skeleton = skeleton.replace("{name}", name).replace("{fn_args}", &fn_args);
+    skeleton = skeleton
+        .replace("{name}", name)
+        .replace("{fn_args}", &fn_args);
 
     // outgoing values (unwrapping from tuple (if necessary) and vec appending)
-    if outgoing_arcs > 1 {
-        let mut outgoing = String::new();
-        for i in 0..outgoing_arcs {
-            if i > 0 {
-            outgoing.push_str(", ")
+    let mut outgoing = String::new();
+
+    println!("Wrapping {}: {:?}", name, outgoing_arcs);
+
+    // do not unpack the tuple if there is only one retval!
+    if outgoing_arcs.len() > 1 {
+        for (index, count) in outgoing_arcs.drain(..).enumerate() {
+            let boxed_arg = format!("Box::from(Box::new(res.{}))", index);
+            let boxed_cloned_arg = format!("Box::from(Box::new(res.{}.clone()))", index);
+
+            // clone the arguments if necessary
+            if count > 1 {
+                outgoing.push_str(
+                    format!("vec![{cloned}], ", cloned = boxed_cloned_arg.repeat(count)).as_ref(),
+                );
+            } else {
+                outgoing.push_str(format!("vec![{}], ", boxed_arg).as_ref());
+            }
         }
-            outgoing.push_str(format!("Box::from(Box::new(res.{}))", i).as_ref());
-        }
+
         skeleton = skeleton.replace("{outgoing_args}", &outgoing);
     } else {
-        skeleton = skeleton.replace("{outgoing_args}", "Box::from(Box::new(res))");
+        // TODO: Remove this hack when issue #1 is resolved
+        if outgoing_arcs.len() == 0 {
+            skeleton = skeleton.replace("{outgoing_args}", "vec![Box::from(Box::new(res))]");
+        } else {
+            let count = outgoing_arcs[0];
+            // the arguments are cloned when necessary
+            if count > 0 {
+                outgoing.push_str(
+                    format!(
+                        "vec![{}]",
+                        "Box::from(Box::new(res.clone())), ".repeat(count)
+                    ).as_ref(),
+                );
+            } else {
+                outgoing.push_str(
+                    format!("vec![{}]", "Box::from(Box::new(res))".repeat(count)).as_ref(),
+                );
+            }
+        }
+
+        skeleton = skeleton.replace("{outgoing_args}", &outgoing);
     }
+
+    // if outgoing_arcs.len() > 1 {
+    //     for i in 0..outgoing_arcs {
+    //         if i > 0 {
+    //         outgoing.push_str(", ")
+    //         }
+    //         outgoing.push_str(format!("Box::from(Box::new(res.{}))", i).as_ref());
+    //     }
+    // } else {
+    //
+    // }
 
     skeleton
 }
 
-fn analyze_dfg(ohuadata: &OhuaData) -> (HashMap<String, (u16, u16, i32)>, HashSet<String>) {
-    let mut function_map: HashMap<String, (u16, u16, i32)> = HashMap::new();
+fn analyze_dfg(
+    ohuadata: &OhuaData,
+) -> (HashMap<String, (usize, Vec<usize>, i32)>, HashSet<String>) {
+    let mut function_map: HashMap<String, (usize, Vec<usize>, i32)> = HashMap::new();
     let mut namespaces = HashSet::new();
 
+    // for each operator in the DFG, check the arcs and count the number of incoming and outgoing arcs
     for op in &ohuadata.graph.operators {
         let mut in_count = 0;
-        let mut out_count = 0;
+        // this vector describes the outgoing arcs, where the first entry is the position and
+        // the second the number of arcs originating from that position
+        let mut out_count: Vec<(i32, usize)> = Vec::new();
 
         for arc in &ohuadata.graph.arcs {
             if arc.target.operator == op.operatorId {
@@ -60,15 +110,45 @@ fn analyze_dfg(ohuadata: &OhuaData) -> (HashMap<String, (u16, u16, i32)>, HashSe
             }
             match &arc.source.val {
                 &ValueType::EnvironmentVal(_) => (),
-                &ValueType::LocalVal(ref src)     => if op.operatorId == src.operator {out_count += 1;}
+                &ValueType::LocalVal(ref src) => if op.operatorId == src.operator {
+                    // perform a sorted insertion into the vector that describes all outgoing arcs
+                    let index = if src.index >= 0 {
+                        src.index
+                    } else {
+                        0
+                    };
+
+                    match out_count.binary_search_by_key(&index, |x| x.0) {
+                        Ok(pos) => {
+                            println!("Incrementing pos {} -- was {}.", pos, out_count[pos].1);
+                            out_count[pos].1 += 1
+                        }
+                        Err(pos) => {
+                            println!("Inserting 1 at {}", pos);
+                            out_count.insert(pos, (index, 1))
+                        }
+                    }
+                },
             }
+            println!("{:?}", out_count);
         }
 
-        let namespace = op.operatorType.qbNamespace.iter().fold(String::new(), |acc, ref x| acc.to_owned() + if acc.len() > 0 {"::"} else {""} + &x);
+        let namespace = op.operatorType
+            .qbNamespace
+            .iter()
+            .fold(String::new(), |acc, ref x| {
+                acc.to_owned() + if acc.len() > 0 { "::" } else { "" } + &x
+            });
 
         namespaces.insert(namespace);
-        let fn_name = String::from(op.operatorType.qbNamespace.last().unwrap().as_str()) + "::" + op.operatorType.qbName.as_str();
-        function_map.insert(fn_name, (in_count, out_count, op.operatorId));
+        let fn_name = String::from(op.operatorType.qbNamespace.last().unwrap().as_str()) + "::"
+            + op.operatorType.qbName.as_str();
+        // transform the vector, get rid of the first tuple entry
+        let out = out_count
+            .drain(..)
+            .unzip::<i32, usize, Vec<i32>, Vec<usize>>()
+            .1;
+        function_map.insert(fn_name, (in_count, out, op.operatorId));
     }
 
     (function_map, namespaces)
@@ -77,7 +157,7 @@ fn analyze_dfg(ohuadata: &OhuaData) -> (HashMap<String, (u16, u16, i32)>, HashSe
 fn get_argument(arg_no: i32) -> String {
     match arg_no {
         0 => String::from("8"),
-        _ => String::new()
+        _ => String::new(),
     }
 }
 
@@ -89,12 +169,21 @@ fn generate_mainarg_wrappers(first_id: i32, ohuadata: &OhuaData) -> (Vec<Operato
 
     for arg_no in 0..ohuadata.mainArity {
         // generate wrapper code
-        let wrapper = String::from(template).replace("{n}", format!("{}", arg_no).as_str()).replace("{argument}", get_argument(arg_no).as_str());
+        let wrapper = String::from(template)
+            .replace("{n}", format!("{}", arg_no).as_str())
+            .replace("{argument}", get_argument(arg_no).as_str());
         wrapper_code.push_str(wrapper.as_str());
 
         // generate new operator for respective argument
         let fn_name = format!("mainarg{}", arg_no);
-        operators.push(Operator {operatorId: first_id + arg_no, operatorType: OperatorType {qbNamespace: vec![], qbName: fn_name.clone(), func: fn_name}});
+        operators.push(Operator {
+            operatorId: first_id + arg_no,
+            operatorType: OperatorType {
+                qbNamespace: vec![],
+                qbName: fn_name.clone(),
+                func: fn_name,
+            },
+        });
     }
 
     (operators, wrapper_code)
@@ -107,7 +196,9 @@ pub fn generate_wrappers(mut ohuadata: OhuaData, target_file: &str) -> Result<Oh
     let skeleton = include_str!("templates/wrappers.rs");
 
     // generate imports
-    let imports = namespaces.iter().fold(String::new(), |acc, ref x| acc + "use " + x + ";\n");
+    let imports = namespaces
+        .iter()
+        .fold(String::new(), |acc, ref x| acc + "use " + x + ";\n");
 
     // generate function wrappers
     let mut func_wrapper = String::new();
@@ -115,12 +206,14 @@ pub fn generate_wrappers(mut ohuadata: OhuaData, target_file: &str) -> Result<Oh
         func_wrapper.push_str(wrap_function(name.as_str(), io.0, io.1).as_str());
 
         // link the function to the ohua data structure
-        if let Ok(pos) = ohuadata.graph.operators.binary_search_by_key(&io.2, |ref op| op.operatorId) {
+        if let Ok(pos) = ohuadata
+            .graph
+            .operators
+            .binary_search_by_key(&io.2, |ref op| op.operatorId)
+        {
             ohuadata.graph.operators[pos].operatorType.func = name.replace("::", "_");
         }
     }
-
-    // let mut altered = ohuadata.clone();
 
     let first_mainarg = (ohuadata.graph.operators.len() + 1) as i32;
     let (mut mainarg_ops, arg_wrappers) = generate_mainarg_wrappers(first_mainarg, &ohuadata);
@@ -128,15 +221,23 @@ pub fn generate_wrappers(mut ohuadata: OhuaData, target_file: &str) -> Result<Oh
 
     for mut arc in ohuadata.graph.arcs.iter_mut() {
         if let ValueType::EnvironmentVal(offset) = arc.source.val {
-            arc.source = ArcSource {s_type: String::from("local"), val: ValueType::LocalVal(ArcIdentifier{operator: first_mainarg + offset, index: -1})};
+            arc.source = ArcSource {
+                s_type: String::from("local"),
+                val: ValueType::LocalVal(ArcIdentifier {
+                    operator: first_mainarg + offset,
+                    index: -1,
+                }),
+            };
         }
     }
 
-    File::create(target_file)?.write_fmt(format_args!("{skel}{imp}{func}{args}", 
-                                                      skel=skeleton,
-                                                      imp=imports,
-                                                      func=func_wrapper,
-                                                      args=arg_wrappers))?;
+    File::create(target_file)?.write_fmt(format_args!(
+        "{skel}{imp}{func}{args}",
+        skel = skeleton,
+        imp = imports,
+        func = func_wrapper,
+        args = arg_wrappers
+    ))?;
 
     Ok(ohuadata)
 }
