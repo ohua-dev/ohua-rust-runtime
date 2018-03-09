4b61d638e1e89fb76668f2fe87fdc5ba6c855197
Felix Wittwer <dev@felixwittwer.de>
2018-03-09 10:44:58 +0100

    Add test case for ohua-dev/ohuac#4

---
+++ testcases/tuple_return/src/main.rs
@@ -0,0 +1,12 @@
+mod ohua_runtime;
+mod tuple_ret;
+
+fn main() {
+    let input = String::from("This is a test string.");
+    let (old, spliced, new) = ohua_runtime::ohua_main(input);
+
+    println!(
+        "Old/new string length: {}/{} -- String: \"{}\"",
+        old, new, spliced
+    );
+}
---
+++ testcases/tuple_return/src/tuple_ret.rs
@@ -0,0 +1,8 @@
+pub fn calculate_string_length(input: String) -> usize {
+    input.len()
+}
+
+pub fn splice_string(input: String) -> String {
+    input += " foo";
+    input
+}
