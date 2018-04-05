3d5cec69c1423800309018ffdb4105a0cb298303
Felix Wittwer <dev@felixwittwer.de>
2018-04-05 15:23:59 +0200

    [skip ci] Update test cases to use new build pipeline

---
+++ testcases/build.rs
@@ -0,0 +1,7 @@
+// BE CAREFUL WHEN CHANGING THIS FILE
+// This build script is part of every testcase in this folder.
+extern crate ohua_rust_runtime;
+
+fn main() {
+    ohua_rust_runtime::run_ohua_build();
+}
