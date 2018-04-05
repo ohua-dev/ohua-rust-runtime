7c137b6e139b5337f1bbacf8fc3244e4613b740a
Felix Wittwer <dev@felixwittwer.de>
2018-04-05 15:19:09 +0200

    Add first two steps to pipeline (ohuac, codegen)
    [skip ci]
    
    This commit outlines the basic build pipeline workflow and implements a
    first wrapper around `ohuac` to integrate all currently implemented
    steps into the pipeline.
    
    - Add `ohuac` module with wrapper code for build and type-dump
    - Rename module to library-friendly syntax

---
+++ src/ohuac.rs
@@ -0,0 +1,107 @@
+//! Wrappers around the `ohuac` binary
+
+use std::path::PathBuf;
+use std::process::Command;
+use std::io::ErrorKind;
+use std::error::Error;
+use std::hash::Hash;
+use std::collections::hash_map::DefaultHasher;
+
+pub struct OhuaProduction {
+    pub src: PathBuf,
+    pub ohuao: PathBuf,
+    pub typedump: PathBuf,
+}
+
+/// Creates a hashed filename, based on the original path's hash
+fn hashed_filename(input_name: &PathBuf) -> String {
+    let mut output_name: String = stringify!(input_name.file_stem().unwrap()).into();
+
+    let mut hasher = DefaultHasher::new();
+    input_name.hash(&mut hasher);
+    output_name += stringify!(hasher.finish());
+
+    output_name
+}
+
+// TODO: Maybe return Hashmap to remove any dupes?
+pub fn generate_dfgs(mut sources: Vec<PathBuf>, target_dir: PathBuf) -> Vec<OhuaProduction> {
+    let mut processed_algos: Vec<OhuaProduction> = Vec::new();
+
+    // run the type dump and object file creation for all ohuac files found
+    for src in sources.drain(..) {
+        let mut dfg_file = target_dir.clone();
+        dfg_file.push(hashed_filename(&src));
+        dfg_file.set_extension("ohuao");
+
+        // build the `ohuao` file
+        let dfg_output = match Command::new("ohuac")
+            .args(&[
+                "build",
+                src.to_str().unwrap(),
+                "--output",
+                dfg_file.to_str().unwrap(),
+            ])
+            .output()
+        {
+            Ok(out) => out,
+            Err(e) => {
+                if e.kind() == ErrorKind::NotFound {
+                    panic!("The `ohuac` executable could not be found. Please make sure to have `ohuac` installed and in your $PATH.");
+                } else {
+                    panic!("Unable to spawn `ohuac`. {}", e.description());
+                }
+            }
+        };
+
+        if !dfg_output.status.success() {
+            panic!(
+                "[Phase 1] `ohuac` failed to process file {}: {}",
+                src.to_str().unwrap(),
+                String::from_utf8(dfg_output.stderr).unwrap()
+            );
+        }
+
+        // build the `type-dump` file
+        let mut type_file = dfg_file.clone();
+        type_file.set_extension("type-dump");
+        let type_output = match Command::new("ohuac")
+            .args(&[
+                "dump-main-type",
+                "rust",
+                src.to_str().unwrap(),
+                "--output",
+                type_file.to_str().unwrap(),
+            ])
+            .output()
+        {
+            Ok(out) => out,
+            Err(e) => {
+                if e.kind() == ErrorKind::NotFound {
+                    panic!("The `ohuac` executable could not be found. Please make sure to have `ohuac` installed and in your $PATH.");
+                } else {
+                    panic!("Unable to spawn `ohuac`. {}", e.description());
+                }
+            }
+        };
+
+        if type_output.status.success() {
+            println!("[Phase 1] Processed file {}", src.to_str().unwrap());
+        } else {
+            panic!(
+                "[Phase 1] `ohuac` failed to process file {}: {}",
+                src.to_str().unwrap(),
+                String::from_utf8(type_output.stderr).unwrap()
+            );
+        }
+
+        // if everything went smoothly, book the file in
+        processed_algos.push(OhuaProduction {
+            src: src,
+            ohuao: dfg_file,
+            typedump: type_file,
+        });
+    }
+
+    processed_algos
+}
