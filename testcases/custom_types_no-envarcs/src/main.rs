349d299ab1fdb44cf07628623b0bc5f2b055b27f
Felix Wittwer <dev@felixwittwer.de>
2018-10-12 13:44:11 +0200

    Port custom_types test case (no env arcs for now)

---
+++ testcases/custom_types_no-envarcs/src/main.rs
@@ -0,0 +1,13 @@
+#![feature(proc_macro_hygiene)]
+extern crate ohua_codegen;
+extern crate ohua_runtime;
+
+mod house;
+
+use house::{EmotionalState, House, Human};
+use ohua_codegen::ohua;
+
+fn main() {
+    #[ohua]
+    custom_types();
+}
