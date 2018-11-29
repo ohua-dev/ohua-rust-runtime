01a3672c2c48ca5d9be9258836d46c6c8a88f1d5
Sebastian Ertel <sebastian.ertel@gmail.com>
2018-11-29 13:42:47 +0100

    complete set operators/functions requested by the updated core compiler.

--- ohua_runtime/src/lang.rs
+++ ohua_runtime/src/lang.rs
@@ -1,17 +1,74 @@
 use std::sync::mpsc::{Receiver, Sender};
 use super::RunError;
+use std::iter::Iterator;
+use std::any::Any;
 
-// TODO the minimal implementation works on the Ord trait
 #[allow(non_snake_case)]
-pub fn smapFun<T: Send>(inp: &Receiver<Vec<T>>, out: &Sender<T>) -> Result<(), RunError> {
-    let vs = inp.recv()?;
-    for v in vs {
-        out.send(v)?;
+pub fn smapFun<T: Any + 'static + Send, S: Iterator<Item = T> + 'static + Send>
+    (inp: &Receiver<S>, data_out: &Sender<T>, ctrl_out: &Sender<(bool, usize)>, collect_out: &Sender<usize>)
+    -> Result<(), RunError> {
+    let data = inp.recv()?;
+    let (_, size) = data.size_hint();
+    match size {
+        Some(s) => {
+            // known size
+            collect_out.send(s)?;
+            ctrl_out.send((true,s))?;
+            for d in data { data_out.send(d)?; }
+        }
+        None => {
+            // unknown size -> generator-style
+            let mut size = 0;
+            for d in data {
+                data_out.send(d)?;
+                ctrl_out.send((false,1))?;
+                size = size +1;
+            }
+            collect_out.send(size)?;
+            ctrl_out.send((true,0))?;
+        }
     }
     Ok(())
 }
 
-// a fully explicit operator version
+pub struct CtrlState<T>{
+    vars: Vec<T>,
+    renew: bool,
+}
+
+pub fn ctrl_state<T: Send + Clone>() -> CtrlState<T> {
+    CtrlState{
+        vars: vec![],
+        renew: true,
+    }
+}
+
+// TODO heterogeneous lists in Rust don't come easy.
+//      we really have to put this operator into our code generation.
+//      this might work for now: https://github.com/lloydmeta/frunk
+pub fn ctrl<T: Send + Clone>(state: &mut CtrlState<T>, ctrl_inp: &Receiver<(bool,isize)>,
+                     vars_inp: &Vec<Receiver<T>>, outs: &Vec<Sender<T>>) -> Result<(), RunError> {
+
+    let (renew_next_time, count) = ctrl_inp.recv()?;
+    if !state.renew {
+        // TODO Turn unwraps into the proper runtime errors
+        let vars:Vec<T> = vars_inp.iter().map(|var_inp| var_inp.recv().unwrap()).collect();
+        state.vars = vars;
+    } else {
+        // renuse the captured vars
+    };
+
+    // update the state
+    state.renew = renew_next_time;
+
+    for _ in 0..count {
+        state.vars.iter()
+                  .zip(outs)
+                  .for_each(|(var, out)| out.send(var.clone()).unwrap());
+    }
+    Ok(())
+}
+
 pub fn collect<T: Send>(n: &Receiver<usize>, data: &Receiver<T>, out: &Sender<Vec<T>>) -> Result<(), RunError> {
     let num = n.recv()?;
     let mut buffered = Vec::new();
@@ -38,44 +95,20 @@ pub fn select<T: Send>(
 }
 
 #[allow(non_snake_case)]
-pub fn oneToN<T: Send + Clone>(n: &Receiver<usize>, val: &Receiver<T>, out: &Sender<T>) -> Result<(), RunError> {
-    // TODO 2 more efficient implementations exist:
-    //      1. send the key and the value once -> requires special input ports that are sensitive to that.
-    //      2. send a batch -> requires input ports to understand the concept of a batch.
-    // feels like the second option is the more general one (but also creates more data).
-    // note: sharing the value is only possible of the function using it, does not mutate it! this can be yet another application for our knowledge base to find out which version to choose.
-    let v = val.recv()?;
-    for _ in 0..(n.recv()?) {
-        out.send(v.clone())?;
+pub fn ifFun(cond: &Receiver<bool>,
+             ctrl_true: &Sender<(bool,isize)>,
+             ctrl_false: &Sender<(bool,isize)>) -> Result<(), RunError> {
+    if cond.recv()? {
+        ctrl_true.send((true, 1))?;
+        ctrl_false.send((true, 0))?;
+    } else {
+        ctrl_true.send((true, 0))?;
+        ctrl_false.send((true, 1))?;
     }
     Ok(())
 }
 
-// that's actually a stateful function
-pub fn size<T>(data: Vec<T>) -> usize {
-    data.len()
-}
-
-// stateful function
-pub fn id<T>(data: T) -> T {
-    data
-}
-
-// reference implementation: https://github.com/ohua-dev/ohua-jvm-runtime/blob/master/src/java/ohua/lang/IfSupport.java
-// issue in core: https://github.com/ohua-dev/ohua-core/issues/21
-pub fn bool(
-    b: &Receiver<bool>,
-    pos: &Sender<bool>,
-    neg: &Sender<bool>,
-    select_input: &Sender<bool>,
-) -> Result<(), RunError> {
-    let v = b.recv()?;
-    pos.send(v)?;
-    neg.send(!v)?;
-    select_input.send(v)?;
-    Ok(())
-}
-
-pub fn seq<T: Send>(_: T) -> bool {
-    true
+#[allow(non_snake_case)]
+pub fn seqFun<T: Send>(_: T) -> (bool,isize) {
+    (true, 1)
 }
