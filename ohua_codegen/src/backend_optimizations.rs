92409291a4f131e4a4c232aa2ca85ac25b16061b
Felix Wittwer <dev@felixwittwer.de>
2019-01-29 11:10:56 +0100

    Fix seq_test using an additional optimization

--- ohua_codegen/src/backend_optimizations.rs
+++ ohua_codegen/src/backend_optimizations.rs
@@ -1,4 +1,4 @@
-use ohua_types::OhuaData;
+use ohua_types::{ArcIdentifier, ArcSource, DirectArc, OhuaData};
 use typedgen::get_out_arcs;
 
 /// After severe issues with `ctrl` operators that have empty argument lists,
@@ -19,7 +19,52 @@ fn remove_unused_ctrl(compiled_algo: &mut OhuaData) {
     compiled_algo.graph.operators = ops;
 }
 
+fn resolve_missing_data_out_ports(compiled_algo: &mut OhuaData) {
+    // find all smapFun operators
+    let op_nums: Vec<i32> = compiled_algo
+        .graph
+        .operators
+        .iter()
+        .filter(|op| {
+            op.operatorType.qbNamespace == vec!["ohua_runtime", "lang"]
+                && op.operatorType.qbName.as_str() == "smapFun"
+        })
+        .map(|op| op.operatorId)
+        .collect();
+
+    // check, if a smapFun operator has been optimized and the `data_out` arc was removed
+    for op_id in op_nums {
+        if compiled_algo
+            .graph
+            .arcs
+            .direct
+            .iter()
+            .find(|arc| match arc.source {
+                ArcSource::Local(ArcIdentifier {
+                    operator: id,
+                    index: 0,
+                }) => id == op_id,
+                _ => false,
+            })
+            .is_none()
+        {
+            // if so, add a DeadEndArc
+            compiled_algo.graph.arcs.direct.push(DirectArc {
+                target: ArcIdentifier {
+                    operator: 0,
+                    index: 0,
+                },
+                source: ArcSource::Local(ArcIdentifier {
+                    operator: op_id,
+                    index: 0,
+                }),
+            });
+        }
+    }
+}
+
 /// Run a set of backend-specific optimizations on the `OhuaData` structure
 pub fn run_backend_optimizations(compiled_algo: &mut OhuaData) {
     remove_unused_ctrl(compiled_algo);
+    resolve_missing_data_out_ports(compiled_algo);
 }
