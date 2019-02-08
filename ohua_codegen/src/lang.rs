1f6bfdf0e20570571a8972e10c4281d7cf2bc876
Justus Adam <dev@justus.science>
2019-02-08 14:52:06 +0100

    Working on recur code generation
    Found the reason for the missing argument, see #30, and a workaround
    Fixed the return type of `recur_n` and the order of the arguments

--- ohua_codegen/src/lang.rs
+++ ohua_codegen/src/lang.rs
@@ -217,22 +217,25 @@ pub mod generate_recur {
             fn #fn_name<#(#arg_types0 : Send),*, #return_type2 : Send>
                 (condition: &Receiver<bool>,
                  result_arc: &Receiver<#return_type0>,
-                 #(#initial_args0 : Receiver<#arg_types1>),*,
-                 #(#loop_args0 : Receiver<#arg_types2>),*,
+                 #(#initial_args0 : &Receiver<#arg_types1>),*,
+                 #(#loop_args0 : &Receiver<#arg_types2>),*,
                  ctrl_arc: &dyn ArcInput<bool>,
                  cont_arc: &dyn ArcInput<(#(#arg_types),*)>,
                  finish_arc: &dyn ArcInput<#return_type1>,
-                ) {
+                ) -> Result<(), RunError>
+            {
+                ctrl_arc.dispatch(true);
+                cont_arc.dispatch(
+                    (#(#initial_args.recv()?),*)
+                );
+                while (condition.recv()?) {
                     ctrl_arc.dispatch(true);
-                    cont_arc.dispatch(
-                        (#(#initial_args.recv()?),*)
-                    );
-                    while (condition.recv()?) {
-                        ctrl_arc.dispatch(true);
-                        cont_arc.dispatch((#(#loop_args.recv()?),*));
-                    }
-                    finish_arc.dispatch(result_arc.recv()?);
+                    cont_arc.dispatch((#(#loop_args.recv()?),*));
                 }
+                ctrl_arc.dispatch(false);
+                finish_arc.dispatch(result_arc.recv()?);
+                Ok(())
+            }
         }
     }
 }
