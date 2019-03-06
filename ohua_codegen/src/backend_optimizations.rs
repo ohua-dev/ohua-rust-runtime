7ed68111fd570e609ca875c1879d7fc4563de96a
Felix Wittwer <dev@felixwittwer.de>
2019-03-06 10:58:21 +0100

    Update .travis.yml, disable unused_ctrl opt

--- ohua_codegen/src/backend_optimizations.rs
+++ ohua_codegen/src/backend_optimizations.rs
@@ -4,21 +4,21 @@ use typedgen::get_out_arcs;
 /// After severe issues with `ctrl` operators that have empty argument lists,
 /// this optimization pass removes any such operators, leaving behind (implicit)
 /// DeadEndArcs
-fn remove_unused_ctrl(compiled_algo: &mut OhuaData) {
-    // FIXME: Delete me once ohua-dev/ohua-core#32 is fixed
-    // need to pull the ops from the struct first due to borrowing
-    let mut ops = vec![];
-    std::mem::swap(&mut compiled_algo.graph.operators, &mut ops);
-
-    // drop operators that are `ctrl` and have no inputs
-    ops.retain(|op| {
-        !(op.operatorType.qbNamespace == vec!["ohua_runtime", "lang"]
-            && op.operatorType.qbName.as_str() == "ctrl"
-            && get_out_arcs(&op.operatorId, &compiled_algo.graph.arcs.direct).is_empty())
-    });
-
-    compiled_algo.graph.operators = ops;
-}
+// fn remove_unused_ctrl(compiled_algo: &mut OhuaData) {
+//     // FIXME: Delete me once ohua-dev/ohua-core#32 is fixed
+//     // need to pull the ops from the struct first due to borrowing
+//     let mut ops = vec![];
+//     std::mem::swap(&mut compiled_algo.graph.operators, &mut ops);
+//
+//     // drop operators that are `ctrl` and have no inputs
+//     ops.retain(|op| {
+//         !(op.operatorType.qbNamespace == vec!["ohua_runtime", "lang"]
+//             && op.operatorType.qbName.as_str() == "ctrl"
+//             && get_out_arcs(&op.operatorId, &compiled_algo.graph.arcs.direct).is_empty())
+//     });
+//
+//     compiled_algo.graph.operators = ops;
+// }
 
 fn resolve_missing_data_out_ports(compiled_algo: &mut OhuaData) {
     // find all smapFun operators
@@ -66,6 +66,6 @@ fn resolve_missing_data_out_ports(compiled_algo: &mut OhuaData) {
 
 /// Run a set of backend-specific optimizations on the `OhuaData` structure
 pub fn run_backend_optimizations(compiled_algo: &mut OhuaData) {
-    remove_unused_ctrl(compiled_algo);
+    // remove_unused_ctrl(compiled_algo);
     resolve_missing_data_out_ports(compiled_algo);
 }
