a5d122831ff896f146e7b0e71c304ba9de79e681
Justus Adam <dev@justus.science>
2019-03-29 11:58:12 +0100

    Added a test case for top level recursion

--- testcases/src/recursion/mod.rs
+++ testcases/src/recursion/mod.rs
@@ -8,3 +8,10 @@ fn test_simple_recursion() {
     let result = recursion::algorithms::simple_recur();
     assert!(result < 0);
 }
+
+#[test]
+fn test_simple_tl_recursion() {
+    #[ohua]
+    let result = recursion::algorithms::simple_tl_recur();
+    assert!(result < 0);
+}
